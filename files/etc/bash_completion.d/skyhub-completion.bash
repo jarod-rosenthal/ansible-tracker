6d445cb230e8f27645ab798cf2f83019350af4bc
Corey Gaspard <cgaspard@gmail.com>
2020-12-22 08:00:10 -0600

    Adding ability to reset gps device and container

--- files/etc/bash_completion.d/skyhub-completion.bash
+++ files/etc/bash_completion.d/skyhub-completion.bash
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-complete -W "help prune start stop restart update purge status monitor mysql resetdb resetconfig" skyhub
+complete -W "help prune start stop restart update purge status monitor mysql resetdb resetconfig resetgps viewgps" skyhub
